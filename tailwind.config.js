8eda27a80a67248d9eb83e2e5d5e26baaacfd1c4
ssafridi786 <ssafridi786@gmail.com>
2025-02-18 11:40:09 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./public/**/*.{html,js}"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      Playfair: "'Playfair display', serif",
+      Lato: "'Lato', sans-serif"
+    }
+  },
+};
+export const plugins = [];
+
